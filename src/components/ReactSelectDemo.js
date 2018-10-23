9fac7329e1ca4dd4e1bd6a2f6131d5089d6594e5
Edgar Mauricio Nomesque Silva <enomesque@belatrix.com>
2018-10-23 13:56:48 -0500

    Select React Async

--- src/components/ReactSelectDemo.js
+++ src/components/ReactSelectDemo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Select from "react-select";
+import AsyncSelect from 'react-select/lib/Async';
 
 const options = [
   { value: "chocolate", label: "Chocolate" },
@@ -7,10 +8,28 @@ const options = [
   { value: "vanilla", label: "Vanilla" }
 ];
 
+const countriesList = async () => {
+    const response = await fetch('https://restcountries.eu/rest/v2/all');
+    const data = await response.json();
+    console.log(data);
+    const countriesArray = data.map( country => {
+        return {'value': country.alpha3Code, 'label': country.name}
+    });
+    console.log(countriesArray);
+    return countriesArray;
+};
+
 const ReactSelectDemo = () => (
   <div>
     <h2>React Select v2</h2>
     <Select isMulti options={options} />
+    <br></br>
+    <h4>Async Select</h4>
+    <AsyncSelect
+          cacheOptions
+          loadOptions={countriesList}
+          defaultOptions
+        />
   </div>
 );
 
